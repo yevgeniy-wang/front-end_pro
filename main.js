446bb7ef84ecdf7036ee54440cb04bfcb76df78b
Yevgeniy Wang <yevgeniy.wang@gmail.com>
2023-05-31 21:08:32 +0300

    little changes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const array = [1, 2, 3, 4, 5, 6, 7];
-const array2 = ['aaa', 'bbb', 'ccc','abc', 'bac', 'cab'];
+const array2 = ['aaa', 'bbb', 'ccc', 'abc', 'bac', 'cab'];
 
-function removeElement(array, value){
+function removeElement(array, value) {
     array.splice(array.indexOf(value), 1)
     return array
 }
