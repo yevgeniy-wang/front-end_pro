d82fbd9d5d36140199dd70db65fc5fb94ccfa6e0
Yevgeniy Wang <yevgeniy.wang@gmail.com>
2023-05-31 21:05:26 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+const array2 = ['aaa', 'bbb', 'ccc','abc', 'bac', 'cab'];
+
+function removeElement(array, value){
+    array.splice(array.indexOf(value), 1)
+    return array
+}
+
+console.log(removeElement(array, 5))
+console.log(removeElement(array2, 'bbb'))
