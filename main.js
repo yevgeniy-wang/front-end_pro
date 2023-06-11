7f927f8c29330bdc727c2a8078cad2227811bc69
Yevgeniy Wang <yevgeniy.wang@gmail.com>
2023-06-12 02:15:59 +0300

    small improvements

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 "use strict";
 
 function pow(num, degree) {
-    if (num === 1 || degree === 0) return num;
+    if (degree === 0) return 1
+    if (num === 1 || num === 0) return num;
     if (degree > 1) return num * pow(num, --degree);
     if (degree < 1) return 1 / num * pow(num, ++degree);
     return num;
 }
 
-console.log(pow(2, 4))
+console.log(pow(2, 3))
+console.log(pow(2, -3))
